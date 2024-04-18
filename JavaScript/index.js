0000b1da0498ed761317b6673aface0f48b9606a
Lezer Zeta <zermoglioleonardo@gmail.com>
2024-04-18 17:12:12 -0300

    Se modifica estructura y lógica del carrusel, se modifican sus imágenes y estilos.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,34 +1,24 @@
-'use strict'
 
-const grande    = document.querySelector('.grande')
-const punto     = document.querySelectorAll('.punto')
+var arrayCarrusel = document.querySelectorAll('.imagenes__carrusel img');
+var arrayPuntos = document.querySelectorAll('.punto');
 
-// Cuando CLICK en punto
-    // Saber la posición de ese punto
-    // Aplicar un transform translateX al grande
-    // QUITAR la clase activo de TODOS puntos
-    // AÑADIR la clase activo al punto que hemos hecho CLICK
+arrayPuntos.forEach((punto, index) => {
+    punto.addEventListener('click', () => {
+        arrayPuntos.forEach(punto => {
+            punto.classList.remove('activo')
+        });
+        punto.classList.add('activo');
+        mostrarImagen(index);
+    });
+});
 
-// Recorrer TODOS los punto
-punto.forEach( ( cadaPunto , i )=> {
-    // Asignamos un CLICK a cadaPunto
-    punto[i].addEventListener('click',()=>{
+function mostrarImagen(index) {
+    desplazar = index * -100;
 
-        // Guardar la posición de ese PUNTO
-        let posicion  = i
-        // Calculando el espacio que debe DESPLAZARSE el GRANDE
-        let operacion = posicion * -25
+    console.log(`El dezplazamiento es de : ${desplazar}`);
+    
+    for (var i = 0; i < arrayCarrusel.length; i++) {
+        arrayCarrusel[i].style.transform = `translateX(${desplazar}%)`;;
+    }
+}
 
-        // MOVEMOS el grand
-        grande.style.transform = `translateX(${ operacion }%)`
-
-        // Recorremos TODOS los punto
-        punto.forEach( ( cadaPunto , i )=>{
-            // Quitamos la clase ACTIVO a TODOS los punto
-            punto[i].classList.remove('activo')
-        })
-        // Añadir la clase activo en el punto que hemos hecho CLICK
-        punto[i].classList.add('activo')
-
-    })
-})
